f1f207dfa1e1b1a07c86ff3cf93c95d28c104ea1
nagane <2742167+nagane@users.noreply.github.com>
2019-11-26 20:13:23 +0900

    wip

---
+++ ABC146/300/AnswerMain.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, x int
+	fmt.Scan(&a, &b, &x)
+	// A x N + B x d(N)
+
+}
+
+func calcDigit(a int, b int, x int) bool {
+	var ans int
+	fmt.Println(ans)
+	return true
+}
