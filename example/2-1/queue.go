7a4261931ea396bce0b7d55b752ee2ae65a1ea97
nagane <2742167+nagane@users.noreply.github.com>
2020-06-07 19:01:00 +0900

    wip

--- example/2-1/queue.go
+++ example/2-1/queue.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	que := q.New(10)
-	fmt.Println(que)
 
 	que.Put(1)
+	fmt.Println(que)
 	que.Put(5)
 
+	fmt.Println(que)
 	fmt.Println(que.Get(5))
+	fmt.Println(que)
 
 }
