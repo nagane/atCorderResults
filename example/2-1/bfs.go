ebc92e73b9d50844ecba6679e3d278e6a89714fb
nagane <2742167+nagane@users.noreply.github.com>
2020-06-12 23:55:28 +0900

    ちゃんとスタート地点取れてなかった

--- example/2-1/bfs.go
+++ example/2-1/bfs.go
@@ -27,7 +27,7 @@ func main() {
 		maze[i] = make([]string, M)
 		for j := 0; j < M; j++ {
 			// TODO: インプットついでにスタートとゴールの座標を取りたい
-			maze[i][j] = str[:j]
+			maze[i][j] = str[j : j+1]
 			//　スタート位置を記憶
 			if maze[i][j] == "S" {
 				sx = i
@@ -47,7 +47,7 @@ func main() {
 	dy := []int{0, 1, 0, -1}
 
 	d := make([][]int, N) //各点までの最短距離の配列
-	for i := 0; 1 < N; i++ {
+	for i := 0; i < N; i++ {
 		d[i] = make([]int, M)
 		for j := 0; j < M; j++ {
 			d[i][j] = INF
@@ -57,9 +57,16 @@ func main() {
 	// スタート地点をキューに入れてその地点を0とする
 	que := q.New(100000000)
 	que.Put(P{sx, sy})
+	d[sx][sy] = 0
 
 	for que.Len() > 0 {
-
+		p, _ := que.Get(1)
+		fmt.Println("pop p")
+		fmt.Println(p)
 	}
+	fmt.Println(gy)
+	fmt.Println(gx)
+	fmt.Println(dy)
+	fmt.Println(dx)
 
 }
