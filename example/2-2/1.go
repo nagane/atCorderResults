456d523ff2a8c7c89f7c7476d46c0b6e3d382a2e
nagane <2742167+nagane@users.noreply.github.com>
2020-07-01 23:52:48 +0900

    貪欲法

--- example/2-2/1.go
+++ example/2-2/1.go
@@ -3,19 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	var c1, c5, c10, c50, c100, c500, A int
-	var C [5]int
+	var A int
+	var C [6]int
 	V := [...]int{1, 5, 10, 50, 100, 500}
-	fmt.Scan(&C[0], &C[1], &C[2], &C[3], &C[4], &C[5], A)
+	fmt.Scan(&C[0], &C[1], &C[2], &C[3], &C[4], &C[5], &A)
 
 	ans := 0
 
 	for i := 5; i >= 0; i-- {
 		var t int
 
-		if A/V[i] > C[i] {
+		if A/V[i] > 0 && 0 < C[i] {
+			if A/V[i] < C[i] {
+				// コインの枚数
+				t = A / V[i]
+				A -= V[i] * (A / V[i])
+			} else {
+				// コインの枚数
+				t = C[i]
+				A -= V[i] * C[i]
+			}
 
+			ans += t
 		}
-
 	}
+
+	fmt.Println(ans)
 }
